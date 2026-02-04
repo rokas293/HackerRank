22f0b32e0450b564749af5c220e1661ff5a124e2
rokas mitka <83891594+rokas293@users.noreply.github.com>
2026-02-04 20:54:39 +0000

    Update and rename Solve_Me_First to Solve_Me_First.py

---
+++ Algorithms/Python/Solve_Me_First.py
@@ -0,0 +1,48 @@
+# Source : https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+# Author : Rokas Mitka
+# Date   : 2/4/2026
+
+# ****************************************************************************************************************************
+# Complete the function solveMeFirst to compute the sum of two integers.
+#
+# Example
+#
+# a = 7
+# b = 3
+#
+# Return 10.
+#
+# Function Description
+#
+# Complete the solveMeFirst function with the following parameters:
+#
+# int a: the first value
+# int b: the second value
+# Returns
+# -int : the sum of a and b
+#
+# Constraints
+#
+# 1 ≤ a ,b ≤ 1000
+#
+# 
+# a = 2
+# b = 3
+#
+# Sample Output
+# 
+# 5
+#
+# Explanation
+#
+# 2 + 3 = 5
+# ****************************************************************************************************************************
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
