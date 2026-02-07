7f416262ee2e243c21760a894e875baac92f8075
rokas mitka <83891594+rokas293@users.noreply.github.com>
2026-02-07 19:59:50 +0000

    Create Mini-Max_Sum.py

---
+++ Algorithms/Python/Mini-Max_Sum.py
@@ -0,0 +1,95 @@
+# Source : https://www.hackerrank.com/challenges/a-very-big-sum/
+# Author : Rokas Mitka
+# Date   : 2/7/2026
+
+# ***********************************************************************************************************************************************
+#
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+#
+# Example
+# arr = [1,3,5,7,9]
+#
+# The minimum sum is 1 + 3 + 5 + 7 = 16 and the maximum sum is 3 + 5 + 7 + 9 = 24. The function prints
+#
+# 16 24
+#
+# Function Description
+#
+# Complete the miniMaxSum function with the following parameter(s):
+#
+# arr[5]: an array of 5 integers
+# Print
+#
+# Print two space-separated integers on one line: the minimum sum and the maximum sum of 4 of 5 elements.No value should be returned.
+#
+# Note For some languages, like C, C++, and Java, the sums may require that you use a long integer due to their size.
+#
+# Input Format
+#
+# A single line of five space-separated integers.
+#
+# Constraints
+#
+#
+# Sample Input
+#
+# 1 2 3 4 5
+# Sample Output
+#
+# 10 14
+# Explanation
+#
+# The numbers are 1, 2, 3, 4, and 5. Calculate the following sums using four of the five integers:
+#
+# Sum everything except 1, the sum is 2 + 3 + 4 + 5 = 14.
+# Sum everything except 2, the sum is 1 + 3 + 4 + 5 = 13.
+# Sum everything except 3, the sum is 1 + 2 + 4 + 5 = 12.
+# Sum everything except 4, the sum is 1 + 2 + 3 + 5 = 11.
+# Sum everything except 5, the sum is 1 + 2 + 3 + 4 = 10.
+# Hints: Beware of integer overflow! Use a 64-bit integer to store the sums.
+#
+# Need help to get started? Try the Solve Me First problem.
+
+# ***********************************************************************************************************************************************
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    
+    total = 0
+    min_val = arr[0]
+    max_val = arr[0]
+    
+    for i in arr:
+        
+        total += i
+        
+        if i < min_val:
+            min_val = i
+            
+        if i > max_val:
+            max_val = i
+            
+    min_sum = total - max_val
+    max_sum = total - min_val
+    
+    print(min_sum, max_sum) 
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
